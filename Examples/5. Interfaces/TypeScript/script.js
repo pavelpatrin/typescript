bd9cc94b2c152e1e28d3205a9dfe8d3075ed6a8f
Pavel Patrin <pavelpat@ya.ru>
2014-06-07 05:20:56 +0400

    + Потерянные файлы

---
+++ Examples/5. Interfaces/TypeScript/script.js
@@ -0,0 +1,22 @@
+/**
+* Created by pavelpat on 03.06.14.
+*/
+
+var MoverShakerImpl = (function () {
+    function MoverShakerImpl() {
+    }
+    MoverShakerImpl.prototype.move = function () {
+    };
+
+    MoverShakerImpl.prototype.shake = function () {
+    };
+
+    MoverShakerImpl.prototype.getStatus = function () {
+        return {
+            speed: 100,
+            frequency: 200
+        };
+    };
+    return MoverShakerImpl;
+})();
+//# sourceMappingURL=script.js.map
